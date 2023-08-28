4336505d7ff37cee6187e32dcc8ef4c38b410dbb
TranCaoVy160924 <vytcse160924@fpt.edu.vn>
2023-08-28 15:19:39 +0700

    create api

---
+++ Application/src/main/java/dxc/assignment/Application.java
@@ -0,0 +1,12 @@
+package dxc.assignment;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "dxc.assignment")
+public class Application {
+
+}
